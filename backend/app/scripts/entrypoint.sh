3c18bb48395c3548e0c7e82a0b5c54d75442572c
IcaroJustino <icaromirandajustino@gmail.com>
2025-12-14 12:50:54 -0300

    fix:dependencias do docker-compose

--- backend/app/scripts/entrypoint.sh
+++ backend/app/scripts/entrypoint.sh
@@ -1,9 +1,5 @@
-
-
-
 #!/bin/sh
 
-# --- Variáveis de Configuração ---
 DB_HOST=${DB_HOST:-db}
 DB_PORT=${DB_PORT:-5432}
 REDIS_HOST=${REDIS_HOST:-redis}
@@ -11,7 +7,6 @@ REDIS_PORT=${REDIS_PORT:-6379}
 MAX_RETRIES=15
 RETRY_COUNT=0
 
-# --- FUNÇÃO DE ESPERA E VERIFICAÇÃO ---
 wait_for_service() {
     local host="$1"
     local port="$2"
@@ -32,21 +27,15 @@ wait_for_service() {
     echo "${service_name} está UP e aceitando conexões."
 }
 
-# --- EXECUÇÃO ---
-
-# 1. Aguarda o PostgreSQL
 wait_for_service "$DB_HOST" "$DB_PORT" "PostgreSQL"
 
-# 2. Aguarda o Redis
 wait_for_service "$REDIS_HOST" "$REDIS_PORT" "Redis"
 
-# 3. Inicialização e Seed do Banco de Dados
 echo "Garantindo que as tabelas existam no DB..."
 python -c "from core.database import init_db; init_db()"
 
 echo "Iniciando o script de seed via SQLAlchemy..."
 python -m scripts.seed
 
-# 4. Inicia o Servidor Principal
 echo "Iniciando servidor Uvicorn..."
 exec "$@"
